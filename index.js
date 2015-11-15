6613a524deee98784f9b3a282ebfbe08c38dc539
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-15 16:08:25 -0500

    Add app.import statements for octicons css and font files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-
 module.exports = {
   name: 'ember-cli-octicons',
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/octicons/octicons/octicons.css');
+    app.import(app.bowerDirectory + '/octicons/octicons/octicons.eot', { destDir: 'assets' });
+    app.import(app.bowerDirectory + '/octicons/octicons/octicons.woff', { destDir: 'assets' });
+    app.import(app.bowerDirectory + '/octicons/octicons/octicons.ttf', { destDir: 'assets' });
+    app.import(app.bowerDirectory + '/octicons/octicons/octicons.svg', { destDir: 'assets' });
   }
 };
