3b69ea55b77bf3e70655d42dde17ef4349db5e67
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-15 15:58:29 -0500

    Remove SCSS import work

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // See https://github.com/ember-cli/ember-cli/issues/4084
-    sassOptions: {
-      extension: 'scss'
-    }
+    // Add options here
   });
 
   /*
