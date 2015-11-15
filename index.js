72171a88a29b7d6fc2a7db7495734ce85ff47327
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-15 15:33:37 -0500

    Add blueprint to install Octicons via Bower

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-octicons'
+  name: 'ember-cli-octicons',
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  }
 };
