d7d16936b72e32005adc39da2dc78997b3c02a83
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-15 15:24:48 -0500

    Add failing CSS test

---
+++ tests/acceptance/css-styles-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | css styles');
+
+test('check that Octicons scss is being applied', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal($('.octicon').css('display'), 'inline-block');
+  });
+});
