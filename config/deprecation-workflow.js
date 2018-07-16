45e73edf54337e3f5ff456eee3e70ebbfb0c710b
Kevin Pfefferle <kevin@pfefferle.co>
2018-07-16 18:41:01 -0400

    Yarn install ember-source@3.3.0 and silence deprecations

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" }
+  ]
+};
