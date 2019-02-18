0645e748fe683956d10e220e6e843fe5082541bc
Kevin Pfefferle <kevin@pfefferle.co>
2019-02-18 16:51:48 -0500

    Temporarily lock Octicons to 8.3

--- blueprints/ember-octicons/index.js
+++ blueprints/ember-octicons/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
-    return this.addPackageToProject('octicons', '^7.2.0');
+    return this.addPackageToProject('octicons', '~8.3.0');
   }
 }
