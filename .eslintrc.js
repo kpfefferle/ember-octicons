2b6adfd2557a74b910951733dff905357b65bfd1
Kevin Pfefferle <kevin@pfefferle.co>
2020-07-13 14:59:33 -0400

    Updates for ember-cli@3.19.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,7 @@ module.exports = {
   env: {
     browser: true
   },
-  rules: {
-    'ember/no-jquery': 'error'
-  },
+  rules: {},
   overrides: [
     // node files
     {
@@ -48,9 +46,7 @@ module.exports = {
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
+      extends: ['plugin:node/recommended']
     }
   ]
 };
